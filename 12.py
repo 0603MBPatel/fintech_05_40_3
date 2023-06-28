91880080afd76514843166bd79bc5cf1f9a4c436
mayur <shreeji.5291@gmail.com>
2023-06-28 08:15:42 +0530

    1.2

---
+++ 12.py
@@ -0,0 +1,16 @@
+# calculator.py
+def add(a, b):
+    return a + b
+
+def subtract(a, b):
+    return a - b
+
+def multiply(a, b):
+    return a * b
+
+def divide(a, b):
+    return a / b
+
+# Example usage
+result = add(5, 3)
+print("Addition:", result)
